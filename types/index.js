07627a69bf12fa91e0bc394d116a04b914665ee1
Michael Cacciatore <mcacciatore116@gmail.com>
2019-07-12 21:09:16 -0400

    add types

---
+++ types/index.js
@@ -0,0 +1,17 @@
+const { GRAPHQL_PATH } = require('../constants');
+
+const {
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLID,
+} = require(GRAPHQL_PATH);
+
+module.exports = {
+  String: GraphQLString,
+  Float: GraphQLFloat,
+  Boolean: GraphQLBoolean,
+  Number: GraphQLInt,
+  ID: GraphQLID,
+};
