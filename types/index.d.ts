1e32f5a2c340ff7ebe90f9dece5b87974c7c49de
Michael Cacciatore <mcacciatore116@gmail.com>
2019-10-16 17:26:08 -0400

    add types for our types and fix graphql import version

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+import { GraphQLBoolean, GraphQLID, GraphQLInt, GraphQLFloat, GraphQLString } from 'graphql';
+
+export const String = GraphQLString;
+export const string = GraphQLString;
+export const Boolean = GraphQLBoolean;
+export const boolean = GraphQLBoolean;
+export const Float = GraphQLFloat;
+export const float = GraphQLFloat;
+export const Integer = GraphQLInt;
+export const integer = GraphQLInt;
+export const ID = GraphQLID;
+export const id = GraphQLID;
